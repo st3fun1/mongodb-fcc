3b706cdc1b9a29714c3d06bfeb9459ee59ad6603
Stefan <stefantimosenco@gmail.com>
2016-11-13 17:47:45 +0000

    solution found

---
+++ insert.js
@@ -0,0 +1,15 @@
+var mongodb = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/learnyoumongo";
+mongodb.connect(url,function(e,db){
+    if(e) throw e;
+    var collection = db.collection("docs");
+    var obj = {
+        firstName: process.argv[2],
+        lastName: process.argv[3]
+    };
+    collection.insert(obj,function(e,data){
+        console.log(JSON.stringify(obj));
+        db.close();
+    });
+    
+})
